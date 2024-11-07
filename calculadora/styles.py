3b751b713a4710dbe8ca1924a65c76152bb0ddf5
VitorReisS <vitor20101983@gmail.com>
2024-11-07 11:16:03 -0300

    Criando um botão com QPushButton no PySide6

--- calculadora/styles.py
+++ calculadora/styles.py
@@ -6,9 +6,9 @@ import qdarktheme
 from variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR, PRIMARY_COLOR)
 
 qss = f"""
-    PushButton[cssClass="specialButton"] {{color: #fff; background: {PRIMARY_COLOR};}}
-    PushButton[cssClass="specialButton"]:hover {{color: #fff; background: {DARKER_PRIMARY_COLOR};}}
-    PushButton[cssClass="specialButton"]:pressed {{color: #fff; background: {DARKEST_PRIMARY_COLOR};}}
+    QPushButton[cssClass="specialButton"] {{color: #fff; background: {PRIMARY_COLOR};}}
+    QPushButton[cssClass="specialButton"]:hover {{color: #fff; background: {DARKER_PRIMARY_COLOR};}}
+    QPushButton[cssClass="specialButton"]:pressed {{color: #fff; background: {DARKEST_PRIMARY_COLOR};}}
 """
 
 def setupTheme():
